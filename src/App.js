f1d3256284bdd2e7c36b7636d95b745066da8951
Rossio Lachos <rossio.lachos28@gmail.com>
2023-08-17 13:11:37 -0400

    add component works

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Intro from "./components/Intro/Intro";
 import Services from "./components/Services/Services";
 import Experience from "./components/Experience/Experience";
+import Works from "./components/Works/works";
+import Portfolio from "./components/Portfolio/Portfolio";
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,8 @@ function App() {
      <Intro/>
      <Services/>
      <Experience/>
+     <Works/>
+     <Portfolio/>
 
     </div>
   );
