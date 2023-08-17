c28b1deffbdfd7d07c86a10bc624eddcb86a530f
Rossio Lachos <rossio.lachos28@gmail.com>
2023-08-16 22:36:16 -0400

    Add: Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar/Navbar";
+import './App.css';
 function App() {
   return (
     <div className="App">
-      I am here with new settings
+     <Navbar/>
     </div>
   );
 }
