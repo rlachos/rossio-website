597a3145d102a73ff9bb3507b413d25785b03534
Rossio Lachos <rossio.lachos28@gmail.com>
2023-08-17 00:29:20 -0400

    Add: Services

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Services.css'
+
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+function Services() {
+  return (
+    <div className="services">
+        {/*left side */}
+        <div className="awesome">
+            <span>My Awesome</span>
+            <span>services</span>
+            <spane>
+          Lorem ispum is simpley dummy text of printing of printing Lorem
+          <br />
+          ispum is simpley dummy text of printing
+        </spane>
+          <button className="button s-button">Download CV</button>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+ 
+         </div>
+        {/* right side*/}
+        <div className="cards">
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Services
