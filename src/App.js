37119e085774d1b7ca88a2e2145ab8f497449be2
Rossio Lachos <rossio.lachos28@gmail.com>
2023-08-19 20:44:20 -0400

    Add toggle component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,20 @@ import Portfolio from "./components/Portfolio/Portfolio";
 import Testimonial from "./components/Testimonials/Testimonial";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import { useContext } from "react";
+import { themeContext } from './context';
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div className="App">
+    <div 
+      className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}
+    
+    >
      <Navbar/>
      <Intro/>
      <Services/>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './Footer.css'
-
+import Insta from '@iconscout/react-unicons/icons/uil-instagram'
+import Linkedin from '@iconscout/react-unicons/icons/uil-linkedin'
+import Github from '@iconscout/react-unicons/icons/uil-github'
+import Wave from '../../img/wave.png'
 const Footer = () => {
 
   return (
-    <div>
-      footer
+    <div className="footer">
+        <img src={Wave} alt="" style={{width: '100%'}}/>
+        <div className="f-content">
+            <span>rossio.lachos@gmail.com</span>
+            <div className="f-icons">
+                <Insta color = 'white' size = '3rem' />
+                <Linkedin color = 'white' size = '3rem' />
+                <Github color = 'white' size = '3rem' />
+            </div>
+        </div>
     </div>
   )
 }
