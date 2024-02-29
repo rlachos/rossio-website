6867539770655202cbb035da40191154876f8755
Rossio Lachos <rossio.lachos28@gmail.com>
2024-02-28 19:01:27 -0500

    fixing

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
       {/* right side form */}
       <div className="c-right">
         <form ref = {form} onSubmit = {sendEmail}  >
-          <input type="text" name="user_name" className="user"  placeholder="Nameeeee"/>
+          <input type="text" name="user_name" className="user"  placeholder="Name"/>
           <input type="email" name="user_email" className="user" placeholder="Email"/>
           <textarea name="message" className="user" placeholder="Message"/>
           <input type="submit" value="Send" className="button"/>
