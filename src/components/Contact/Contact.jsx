8680d220104102e6cae1bdef2797e26f043dd543
Rossio Lachos <rossio.lachos28@gmail.com>
2024-02-28 19:00:04 -0500

    Adding words

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -37,11 +37,11 @@ const Contact = () => {
       {/* right side form */}
       <div className="c-right">
         <form ref = {form} onSubmit = {sendEmail}  >
-          <input type="text" name="user_name" className="user"  placeholder="Name"/>
+          <input type="text" name="user_name" className="user"  placeholder="Nameeeee"/>
           <input type="email" name="user_email" className="user" placeholder="Email"/>
           <textarea name="message" className="user" placeholder="Message"/>
           <input type="submit" value="Send" className="button"/>
-          <span>{done && "Thanks for contacting meeeeeee"}</span>
+          <span>{done && "Thanks for contacting"}</span>
           <div
             className="blur c-blur1"
             style={{ background: "var(--purple)" }}
